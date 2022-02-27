cc9ed2ba14594e281c7d2bf6d94ec929b4f4d38a
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-27 13:32:36 +0600

    added: doctors route

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -1,10 +1,13 @@
 from ctypes.wintypes import tagMSG
 from sys import prefix
 from fastapi import APIRouter
+
+from api.v1.endpoints import doctors
 from .endpoints import users, roles
 
 api_router = APIRouter()
 
 
 api_router.include_router(users.router, prefix='', tags=['Users'])
-api_router.include_router(roles.router, prefix='/role', tags=['Roles'])
+api_router.include_router(roles.router, prefix='/roles', tags=['Roles'])
+api_router.include_router(doctors.router, prefix='/doctors', tags=['Doctors'])
