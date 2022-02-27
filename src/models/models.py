b1dc60f274ca2b7f2d8b3917e0be09299958cf89
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-27 13:33:12 +0600

    created: doctors model created

--- src/models/models.py
+++ src/models/models.py
@@ -14,6 +14,7 @@ class User(BaseModel):
     role_id = Column(Integer, ForeignKey("roles.id"))
 
     role = relationship("Role", back_populates="user")
+    doctor = relationship("Doctor", back_populates="user_doctor")
 
 
 class Role(BaseModel):
@@ -21,3 +22,13 @@ class Role(BaseModel):
     name = Column(String(100), nullable=False, unique=True)
 
     user = relationship("User", back_populates="role")
+
+
+class Doctor(BaseModel):
+    __tablename__ = "doctors"
+    user_id = Column(Integer, ForeignKey("users.id"))
+    bmdc = Column(String(100), nullable=False, unique=True)
+    left_header = Column(String(255), nullable=True)
+    right_header = Column(String(255), nullable=True)
+
+    user_doctor = relationship("User", back_populates="doctor")
