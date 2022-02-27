2292df32627f6dd38d4585488c579b24c261de41
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-27 13:34:43 +0600

    added: doctors added in __init__

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,3 +1,4 @@
 from .base import *
 from .users import *
 from .roles import *
+from .doctors import *
--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,3 +1,4 @@
 from .base import *
 from .roles import *
 from .users import *
+from .doctors import *
