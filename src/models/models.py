5dc511f6fea7572ff8a2a90b8d8fa289b59d22d4
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-27 17:36:05 +0600

    added: patient and userdetails

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, Boolean, String
+from sqlalchemy import Column, ForeignKey, Integer, Boolean, String, Text, Date
 from models import BaseModel
 from sqlalchemy.orm import relationship
 
@@ -15,6 +15,7 @@ class User(BaseModel):
 
     role = relationship("Role", back_populates="user")
     doctor = relationship("Doctor", back_populates="user_doctor")
+    patient = relationship("Patient", back_populates="user_patient")
 
 
 class Role(BaseModel):
@@ -22,6 +23,18 @@ class Role(BaseModel):
     name = Column(String(100), nullable=False, unique=True)
 
     user = relationship("User", back_populates="role")
+
+
+class UserDetail(BaseModel):
+    __tablename__ = "user_details"
+    country = Column(String(255), nullable=True)
+    division = Column(String(50), nullable=True)
+    district = Column(String(50), nullable=True)
+    post_code = Column(String(20), nullable=True)
+    sub_district = Column(String(50), nullable=True)
+    nid = Column(String(100), nullable=True)
+    dob = Column(Date, nullable=True)
+    blood_group = Column(String(5), nullable=True)
 
 
 class Doctor(BaseModel):
@@ -32,3 +45,16 @@ class Doctor(BaseModel):
     right_header = Column(String(255), nullable=True)
 
     user_doctor = relationship("User", back_populates="doctor")
+
+
+class Patient(BaseModel):
+    __tablename__ = "patients"
+    user_id = Column(Integer, ForeignKey("users.id"))
+    bio = Column(Text, nullable=True)
+    nid = Column(String(100), nullable=True, unique=True)
+    marital_status = Column(String(20), nullable=True)
+    occupation = Column(String(255), nullable=True)
+    height = Column(Integer, nullable=True)
+    weight = Column(Integer, nullable=True)
+
+    user_patient = relationship("User", back_populates="patient")
