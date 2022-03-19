4cc96604f841f08b664e45ec399f9809e80b82da
sajidurshajib <sajidur.inbox@gmail.com>
2022-03-19 23:12:28 +0600

    created: cc list service

---
+++ src/services/ep_chief_complaints.py
@@ -0,0 +1,20 @@
+from exceptions import ServiceResult, AppException
+from services import BaseService
+from repositories import ep_chief_complaints_list_repo
+from models import EpChiefComplaintsList
+from schemas import CcIn, CcUpdate
+from sqlalchemy.orm import Session
+from fastapi import status
+
+
+class EpCcListService(BaseService[EpChiefComplaintsList, CcIn, CcUpdate]):
+    def search_by_cc(self, db: Session, search_str: str, skip: int, limit: int):
+        cc = self.repo.search_by_cc(db, search_str, skip, limit)
+        if not cc:
+            return ServiceResult(AppException.NotFound("Patient not found"))
+        else:
+            return ServiceResult(cc, status_code=status.HTTP_200_OK)
+
+
+ep_chief_complaints_list_service = EpCcListService(
+    EpChiefComplaintsList, ep_chief_complaints_list_repo)
