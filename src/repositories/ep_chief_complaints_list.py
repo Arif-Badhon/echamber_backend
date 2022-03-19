4335a4adc42f2c88733ae8066805f5005ad751bf
sajidurshajib <sajidur.inbox@gmail.com>
2022-03-19 23:11:36 +0600

    created: cc list repo

---
+++ src/repositories/ep_chief_complaints_list.py
@@ -0,0 +1,15 @@
+from repositories import BaseRepo
+from models import EpChiefComplaintsList
+from schemas import CcIn, CcUpdate
+from sqlalchemy.orm import Session
+
+
+class EpCcListRepo(BaseRepo[EpChiefComplaintsList, CcIn, CcUpdate]):
+
+    def search_by_cc(self, db: Session, search_str: str, skip: int = 0, limit: int = 10):
+        query = db.query(self.model).filter(
+            self.model.cc.like(f"%{search_str}%")).offset(skip).limit(limit).all()
+        return query
+
+
+ep_chief_complaints_list_repo = EpCcListRepo(EpChiefComplaintsList)
