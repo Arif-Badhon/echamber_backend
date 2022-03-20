cab85ddda1c0e47124f2a02ebcd6cbb69aea2070
sajidurshajib <sajidur.inbox@gmail.com>
2022-03-20 13:42:11 +0600

    change: port 3000 into 8000

--- src/main.py
+++ src/main.py
@@ -65,4 +65,4 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1",
-                port=3000, reload=True, log_level="info")
+                port=8000, reload=True, log_level="info")
