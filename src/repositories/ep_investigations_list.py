fec71bf32c84d3ee5118225458115cf135abc367
sajidurshajib <sajidur.inbox@gmail.com>
2022-04-06 13:02:45 +0600

    created: investigations list

--- src/repositories/ep_investigations_list.py
+++ src/repositories/ep_investigations_list.py
@@ -1,1 +1,14 @@
 from repositories import BaseRepo
+from models import EpInvestigationList
+from schemas import InvIn, InvUpdate
+from sqlalchemy.orm import Session
+
+
+class EpInvListRepo(BaseRepo[EpInvestigationList, InvIn, InvUpdate]):
+    def search_by_inv(self, db: Session, search_str: str, skip: int = 0, limit: int = 10):
+        query = db.query(self.model).filter(
+            self.model.investigation.like(f"%{search_str}%")).offset(skip).limit(limit).all()
+        return query
+
+
+ep_investigations_list_repo = EpInvListRepo(EpInvestigationList)
