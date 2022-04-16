4125c861c5d5573dfb7b994d854cc82b0c357c9d
sajidurshajib <sajidur.inbox@gmail.com>
2022-04-16 08:32:17 +0600

    added: doctors chamber

--- src/api/v1/routes.py
+++ src/api/v1/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .endpoints import users, roles, doctors, patients, user_details, patient_indicators, ep_medicines, ep_chief_complaints, ep_investigations, ep_advices
+from .endpoints import doctor_chambers, users, roles, doctors, patients, user_details, patient_indicators, ep_medicines, ep_chief_complaints, ep_investigations, ep_advices
 
 api_router = APIRouter()
 
@@ -8,6 +8,7 @@ api_router.include_router(users.router, prefix='', tags=['Users'])
 api_router.include_router(user_details.router, prefix='/user/details', tags=['User Details'])
 api_router.include_router(roles.router, prefix='/roles', tags=['Roles'])
 api_router.include_router(doctors.router, prefix='/doctors', tags=['Doctors'])
+api_router.include_router(doctor_chambers.router, prefix='/doctors/chamber', tags=['Doctor\'s Chamber'])
 api_router.include_router( patients.router, prefix='/patients', tags=['Patients'])
 api_router.include_router(patient_indicators.router, prefix='/patient/indicators', tags=['Patient Indicators'])
 api_router.include_router(ep_medicines.router, prefix='/medicines', tags=['Medicines'])
