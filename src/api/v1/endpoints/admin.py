106393c2db2d765bfddb77ce3907163b12530039
sajidurshajib <sajidur.inbox@gmail.com>
2022-04-19 16:04:18 +0600

    added: doctor list api

--- src/api/v1/endpoints/admin.py
+++ src/api/v1/endpoints/admin.py
@@ -1,10 +1,11 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from db import get_db
 from exceptions.service_result import handle_result
-from schemas import UserOut, UserCreateWitoutRole
+from schemas import UserOut, UserCreateWitoutRole, UserDoctorOut
 from sqlalchemy.orm import Session
 from services import admin_service
-from api.v1.auth_dependcies import logged_in_admin, logged_in_moderator
+from api.v1.auth_dependcies import logged_in_admin, logged_in_admin_moderator, logged_in_moderator
 
 
 router = APIRouter()
@@ -30,3 +31,15 @@ def moderator_auth(moderator: Session = Depends(logged_in_moderator)):
 def moderator_create(data_in: UserCreateWitoutRole, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin)):
     moderator = admin_service.signup_moderator(db, data_in=data_in)
     return handle_result(moderator)
+
+
+@router.get('/inactive/doctors', response_model=List[UserDoctorOut])
+def doctors_list(db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin_moderator)):
+    docs = admin_service.doctor_inactive_list(db)
+    return handle_result(docs)
+
+
+@router.put('/docto/active', response_model=UserOut)
+def doctor_active(id: int, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin_moderator)):
+    doc = admin_service.doctor_active_id(db=db, id=id)
+    return handle_result(doc)
