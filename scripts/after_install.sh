69fc44f31ccc88549b2adb2690c398688178d781
atiqur <info@awswithatiq.com>
2022-05-09 09:40:35 +0000

    pip3 install added

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,6 +4,8 @@ cd /var/www/echamber/src
 
 source /var/www/echamber/echamber/bin/activate
 
+pip3 install -r /var/www/echamber/requirements.txt
+
 alembic upgrade head
 
 sudo supervisorctl restart all
