3652ccbd48948e2a55c97faf0423ca3535726059
sajidurshajib <sajidur.inbox@gmail.com>
2022-05-18 13:09:23 +0600

    fix: all activity count

--- src/repositories/admin_panel_activity.py
+++ src/repositories/admin_panel_activity.py
@@ -13,15 +13,18 @@ class AdminPanelActivityRepo(BaseRepo[AdminPanelActivity, AdminPanelActivityIn, 
         return activity
 
     def actiity_log_all(self, db: Session, skip: int = 0, limit: int = 15):
+        activity_all = db.query(self.model).order_by(desc(self.model.created_at)).all()
         activity = db.query(self.model).order_by(desc(self.model.created_at)).offset(skip).limit(limit).all()
         activity_data = []
+
         for i in activity:
             user_name = users_repo.get_one(db=db,id=i.user_id).name
             user_phone = users_repo.get_one(db=db, id=i.user_id).phone
             i.user_name = user_name
             i.user_phone = user_phone
             activity_data.append(i)
-        results = len(activity_data) 
+            
+        results = len(activity_all) 
         return [{"results":results}, activity_data]
 
 
