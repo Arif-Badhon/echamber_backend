d2dbc7077ecb9d68ee230ecdabc1e8efe1bc44e2
sajidurshajib <sajidur.inbox@gmail.com>
2022-05-19 17:25:12 +0600

    msg: service order continue

--- src/schemas/service_order.py
+++ src/schemas/service_order.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 from pydantic import BaseModel
 from typing import Optional
 
@@ -6,28 +6,28 @@ from typing import Optional
 class ServiceOrderBase(BaseModel):
     service_name: str
     patient_id: int
-    order_placement: date
-    order_completion: date
+    order_placement: datetime
+    order_completion: datetime
     order_value: int
     discount_percent: int
     payable_amount: int
     payment_customer: int
     payment_pending: int
-    payment_date: date
+    payment_date: datetime
     payment_method: str
     service_provider_type: str
     service_provider_id: int
     service_provider_fee: int
     service_provider_fee_paid: int
     service_provider_fee_pending: int
-    service_provider_fee_last_update: date
+    service_provider_fee_last_update: datetime
     service_provider_fee_status: str
     referral_type: str
     referral_id: int
     referral_provider_fee: int
     referral_provider_fee_paid: int
     referral_provider_fee_pending: int
-    referral_provider_fee_last_update: date
+    referral_provider_fee_last_update: datetime
     referral_provider_fee_status: str
     current_address: str
     remarks: str
@@ -38,28 +38,28 @@ class ServiceOrderIn(ServiceOrderBase):
 class ServiceOrderUpdate(BaseModel):
     service_name: Optional[str] = None
     patient_id: Optional[int] = None
-    order_placement: Optional[date] = None
-    order_completion: Optional[date] = None
+    order_placement: Optional[datetime] = None
+    order_completion: Optional[datetime] = None
     order_value: Optional[int] = None
     discount_percent: Optional[int] = None
     payable_amount: Optional[int] = None
     payment_customer: Optional[int] = None
     payment_pending: Optional[int] = None
-    payment_date: Optional[date] = None
+    payment_date: Optional[datetime] = None
     payment_method: Optional[str] = None
     service_provider_type: Optional[str] = None
     service_provider_id: Optional[int] = None
     service_provider_fee: Optional[int] = None
     service_provider_fee_paid: Optional[int] = None
     service_provider_fee_pending: Optional[int] = None
-    service_provider_fee_last_update: Optional[date] = None
+    service_provider_fee_last_update: Optional[datetime] = None
     service_provider_fee_status: Optional[str] = None
     referral_type: Optional[str] = None
     referral_id: Optional[int] = None
     referral_provider_fee: Optional[int] = None
     referral_provider_fee_paid: Optional[int] = None
     referral_provider_fee_pending: Optional[int] = None
-    referral_provider_fee_last_update: Optional[date] = None
+    referral_provider_fee_last_update: Optional[datetime] = None
     referral_provider_fee_status: Optional[str] = None
     current_address: Optional[str] = None
     remarks: Optional[str] = None
--- src/services/service_order.py
+++ src/services/service_order.py
@@ -1,6 +1,13 @@
+from sqlalchemy.orm import Session
 from services import BaseService
-from repositories import service_order_repo
+from repositories import service_order_repo, admin_panel_activity_repo
 from models import ServiceOrder
 from schemas import ServiceOrderIn, ServiceOrderUpdate
 
-service_order_service = BaseService[ServiceOrder,ServiceOrderIn, ServiceOrderUpdate](ServiceOrder, service_order_repo)
+
+class ServiceOrderService(BaseService[ServiceOrder,ServiceOrderIn, ServiceOrderUpdate]):
+    
+    def created_by_employee(self, db: Session,):
+        return
+
+service_order_service = ServiceOrderService(ServiceOrder, service_order_repo)
