2f030ea9b94bf060799336f9eb6138e6a5172dd2
sajidurshajib <sajidur.inbox@gmail.com>
2022-05-21 10:42:06 +0600

    fix: route serial

--- src/api/v1/endpoints/admin.py
+++ src/api/v1/endpoints/admin.py
@@ -29,15 +29,15 @@ def activity_log(skip:int=0, limit:int=15, db:Session = Depends(get_db), current
     return handle_result(activity)
 
 
-@router.get('/activity/log/{user_id}', response_model=List[Union[ResultInt, List[AdminPanelActivityOut]]])
-def activity_log( user_id: int, skip:int=0, limit:int=15, db:Session = Depends(get_db), current_user: Session = Depends(logged_in)):
-    activity = admin_service.activity_log(db=db, user_id=user_id, skip=skip, limit=limit)
-    return handle_result(activity)
-
-
 @router.get('/activity/log/all', response_model=List[Union[ResultInt, List[AdminPanelActivityAllOut]]])
 def activity_log( skip:int=0, limit:int=15, db:Session = Depends(get_db), current_user: Session = Depends(logged_in)):
     activity = admin_service.activity_log_all(db=db, skip=skip, limit=limit)
+    return handle_result(activity)
+
+
+@router.get('/activity/log/{user_id}', response_model=List[Union[ResultInt, List[AdminPanelActivityOut]]])
+def activity_log( user_id: int, skip:int=0, limit:int=15, db:Session = Depends(get_db), current_user: Session = Depends(logged_in)):
+    activity = admin_service.activity_log(db=db, user_id=user_id, skip=skip, limit=limit)
     return handle_result(activity)
 
 
