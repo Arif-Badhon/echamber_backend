53ca7a7c737ef0921eac2bf45c59782051c76013
sajidurshajib <sajidur.inbox@gmail.com>
2022-07-03 14:02:54 +0600

    added: service order by patient

--- src/api/v1/endpoints/service_order.py
+++ src/api/v1/endpoints/service_order.py
@@ -11,15 +11,25 @@ from services import service_order_service, medicine_order_service
 
 router = APIRouter()
 
-@router.get('/', response_model=List[Union[ResultInt,List[ServiceOrderOut]]], description='Access: employee')
-def all_service_order(skip:int = 0, limit:int=15 ,db: Session = Depends(get_db), current_user: Session = Depends(logged_in_employee)):
-    data = service_order_service.all_service_order(db=db, skip=skip, limit=limit)
+
+@router.get('/', response_model=List[Union[ResultInt, List[ServiceOrderOut]]], description='Access: employee')
+def all_service_order(skip: int = 0, limit: int = 15, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_employee)):
+    data = service_order_service.all_service_order(
+        db=db, skip=skip, limit=limit)
     return handle_result(data)
+
 
 @router.get('/{id}', response_model=ServiceOrderOut)
 def single_service(id: int, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_employee)):
     single = service_order_service.get_one(db=db, id=id)
     return handle_result(single)
+
+
+@router.get('/patient/{id}', response_model=List[Union[ResultInt, List[ServiceOrderOut]]])
+def patient_services(id: int, skip: int = 0, limit: int = 15, db: Session = Depends(get_db)):
+    pt = service_order_service.get_by_key(
+        db=db, skip=skip, limit=limit, descending=True, count_results=True, patient_id=id)
+    return handle_result(pt)
 
 
 @router.patch('/{id}', response_model=ServiceOrderOut)
@@ -28,20 +38,21 @@ def update_service(id: int, data_update: ServiceOrderUpdate, db: Session = Depen
     return handle_result(up)
 
 
-
 @router.post('/medicine', response_model=AdminPanelActivityOut, description='Access: admin and moderator')
-def medicine_order(data_in:List[Union[ServiceOrderIn, List[MedicineOrderIn]]], db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin_moderator)):
-    medicine_order = service_order_service.medicine_order(db=db, data_in=data_in, user_id=current_user.id)
+def medicine_order(data_in: List[Union[ServiceOrderIn, List[MedicineOrderIn]]], db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin_moderator)):
+    medicine_order = service_order_service.medicine_order(
+        db=db, data_in=data_in, user_id=current_user.id)
     return handle_result(medicine_order)
 
 
 @router.get('/medicine/{service_id}', response_model=List[Union[ResultInt, List[MedicineOrderOut]]])
-def medicine_list_by_service(service_id: int, skip:int=0, limit:int=15 ,db: Session = Depends(get_db), current_user:Session=Depends(logged_in_employee)):
-    data = medicine_order_service.get_by_key(db=db, skip=skip, limit=limit, descending=False, count_results=True, service_order_id=service_id)
+def medicine_list_by_service(service_id: int, skip: int = 0, limit: int = 15, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_employee)):
+    data = medicine_order_service.get_by_key(
+        db=db, skip=skip, limit=limit, descending=False, count_results=True, service_order_id=service_id)
     return handle_result(data)
 
 
 @router.patch('/medicine/update/{id}', response_model=MedicineOrderOut)
-def update_medicine(id:int, data_update:MedicineOrderUpdate, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin_crm)):
+def update_medicine(id: int, data_update: MedicineOrderUpdate, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin_crm)):
     up = medicine_order_service.update(db=db, id=id, data_update=data_update)
     return handle_result(up)
