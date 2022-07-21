501af8476f0cd3c2692d645ca5b30d1404509943
sajidurshajib <sajidur.inbox@gmail.com>
2022-07-21 16:49:10 +0600

    doc: is active

--- src/repositories/doctors_search.py
+++ src/repositories/doctors_search.py
@@ -12,16 +12,19 @@ from .roles import roles_repo
 class DoctorsSearchRepo(BaseRepo[User, UserBase, UserUpdate]):
 
     def doc_search_by_chamber_loc(self, db: Session, skip: int, limit: int, district: str):
-        data = db.query(User).join(DoctorChamber, DoctorChamber.user_id == User.id).filter(DoctorChamber.district.like(f"%{district}%")).offset(skip).limit(limit).all()
+        data = db.query(User).join(DoctorChamber, DoctorChamber.user_id == User.id).filter(User.is_active == True).filter(DoctorChamber.district.like(f"%{district}%")).offset(skip).limit(limit).all()
         return data
 
     def doc_search_by_speciality(self, db: Session, skip: int, limit: int, speciality: str):
-        data = db.query(User).join(DoctorSpeciality, DoctorSpeciality.user_id == User.id).filter(DoctorSpeciality.speciality.like(f"%{speciality}%")).offset(skip).limit(limit).all()
+        data = db.query(User).join(
+            DoctorSpeciality, DoctorSpeciality.user_id == User.id).filter(
+            User.is_active == True).filter(
+            DoctorSpeciality.speciality.like(f"%{speciality}%")).offset(skip).limit(limit).all()
         return data
 
     def doc_search_by_name(self, db: Session, skip: int, limit: int, name: str):
         role_id = roles_repo.search_name_id(db=db, name='doctor')
-        data = db.query(self.model).filter(self.model.role_id == role_id).filter(self.model.name.like(f"%{name}%")).offset(skip).limit(limit).all()
+        data = db.query(self.model).filter(self.model.role_id == role_id).filter(User.is_active == True).filter(self.model.name.like(f"%{name}%")).offset(skip).limit(limit).all()
         return data
 
 
