0893be77b120a7af916205c8d8f5994ec44f9d7a
sajidurshajib <sajidur.inbox@gmail.com>
2022-07-26 12:02:18 +0600

    added: healthplan in patient end

--- src/api/v1/endpoints/patients.py
+++ src/api/v1/endpoints/patients.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from api.v1.auth_dependcies import logged_in_patient
 from exceptions import handle_result
-from schemas import PatientOut, PatientSignup, UserDetailOut, NewPasswordIn, Token, ServiceOrderOut, ResultInt, AdminPanelActivityOut, ServiceOrderIn, MedicineOrderIn
+from schemas import PatientOut, PatientSignup, UserDetailOut, NewPasswordIn, Token, ServiceOrderOut, ResultInt, AdminPanelActivityOut, ServiceOrderIn, MedicineOrderIn, HealthPlanForPatientWithService, HealthPlanForPatientOut
 from db import get_db
 from schemas import PatientBase, UserOut, UserOutAuth
 from schemas.medicine_order import MedicineOrderOut
-from services import patients_service, service_order_service, medicine_order_service
+from services import patients_service, service_order_service, medicine_order_service, health_plan_for_patient_service
 
 router = APIRouter()
 
@@ -54,3 +54,15 @@ def medicine_order(data_in: List[Union[ServiceOrderIn, List[MedicineOrderIn]]], 
 def medicine_order_by_service(service_id: int, skip: int = 0, limit: int = 15, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_patient)):
     data = medicine_order_service.get_by_key(db=db, skip=skip, limit=limit, descending=False, count_results=True, service_order_id=service_id)
     return handle_result(data)
+
+
+@router.post('/healthplan/subscribe', response_model=AdminPanelActivityOut)
+def health_plan(data_in: HealthPlanForPatientWithService, voucher_code: str, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_patient)):
+    data = health_plan_for_patient_service.subscribe_with_service(db=db, data_in=data_in, voucher_code=voucher_code, employee_id=current_user.id)
+    return handle_result(data)
+
+
+@router.get('/healthplan/subscribe/{service_id}', response_model=List[HealthPlanForPatientOut])
+def healthplan_subscription_by_service(service_id: int, skip: int = 0, limit: int = 10, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_patient)):
+    hp = health_plan_for_patient_service.get_by_key(db=db, skip=skip, limit=limit, descending=False, count_results=False, service_order_id=service_id)
+    return handle_result(hp)
