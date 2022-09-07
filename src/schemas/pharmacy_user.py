631b7cef3128589f3ecfce456bdcc311e04bf1f8
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-07 11:13:24 +0600

    added: pharmacy user schema

---
+++ src/schemas/pharmacy_user.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class PharmacyUserBase(BaseModel):
+    user_id: int
+    pharmacy_id: int
+
+class PharmacyUserIn(PharmacyUserBase):
+    pass
+
+class PharmacyUserWithPharmacyID(PharmacyUserBase):
+    pharmacy_id: int
+
+class PharmacyUserUpdate(BaseModel):
+    user_id: int
+
+class PharmacyUserOut(PharmacyUserBase):
+    id: int
+    user_id: int
+    pharmacy_id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
