b53c44970a433603499644eaca6c48644506bf03
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-14 11:17:53 +0600

    modified: change in model

--- src/schemas/pharmacy_purchase_order.py
+++ src/schemas/pharmacy_purchase_order.py
@@ -3,14 +3,17 @@ from pydantic import BaseModel
 from typing import Optional
 
 class PharmacyPurchaseOrderBase(BaseModel):
-    total_amount: Optional[float] = None
+    total_amount_dp: Optional[float] = None
+    discount = Optional[float] = None
+    payable_amount:Optional[float] = None
     paid_amount: Optional[float] = None
     due_amount: Optional[float] = None
-    remarks: Optional[str] = None
+    subtotal_amount: Optional[float] = None
+    pharmaceuticals_name_id: Optional[int] = None
     purchase_number: Optional[str] = None
-    pharmaceuticals_id: Optional[int] = None
     user_id: Optional[int] = None
     pharmacy_id: Optional[int] = None
+    remarks: Optional[str] = None
 
 class PharmacyPurchaseOrderIn(PharmacyPurchaseOrderBase):
     pass
