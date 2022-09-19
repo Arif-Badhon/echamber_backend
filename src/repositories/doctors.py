c65ba4bd4348e78125ace62bf2625e94dfd2a06f
sajidurshajib <sajidur.inbox@gmail.com>
2022-09-19 16:17:49 +0600

    change: docor details

--- src/repositories/doctors.py
+++ src/repositories/doctors.py
@@ -3,9 +3,10 @@ from models import Doctor
 from schemas import DoctorIn, DoctorUpdate
 from sqlalchemy.orm import Session
 
+
 class DoctorRepo(BaseRepo[Doctor, DoctorIn, DoctorUpdate]):
     def get_by_user_id(self, db: Session, user_id: int):
-        return db.query(self.model).filter(self.model.user_id==user_id).first()
+        return db.query(self.model).filter(self.model.user_id == user_id).first()
 
 
 doctors_repo = DoctorRepo(Doctor)
