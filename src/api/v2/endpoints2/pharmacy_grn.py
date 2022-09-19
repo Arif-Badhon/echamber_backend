364b188bd647dfd5dbfedb261813a021cee89df0
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-19 17:27:44 +0600

    added: response model

--- src/api/v2/endpoints2/pharmacy_grn.py
+++ src/api/v2/endpoints2/pharmacy_grn.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends
 from schemas import PharmacyGrnWithSingleGrn
 from db import get_db
 from sqlalchemy.orm import Session
+from schemas.pharmacy_grn import PharmacyGrnOut
 from services import pharmacy_grn_service
 from exceptions.service_result import handle_result
 
 router = APIRouter()
 
-@router.post("/")
+@router.post("/", response_model=PharmacyGrnOut)
 def grn_with_single_grn(data_in: PharmacyGrnWithSingleGrn, db: Session = Depends(get_db)):
     grn = pharmacy_grn_service.submit(db=db, data_in=data_in)
     return handle_result (grn)
--- src/api/v2/endpoints2/pharmacy_purchase_order.py
+++ src/api/v2/endpoints2/pharmacy_purchase_order.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends
 from schemas import PharmacyPurchaseOrderWithSingleOrder
 from db import get_db
 from sqlalchemy.orm import Session
+from schemas.pharmacy_purchase_order import PharmacyPurchaseOrderOut
 from services import pharmacy_purchase_order_service
 from exceptions.service_result import handle_result
 
 router = APIRouter()
 
-@router.post("/")
+@router.post("/", response_model=PharmacyPurchaseOrderOut)
 def pharmacy_purchase_order_with_singleorder(data_in: PharmacyPurchaseOrderWithSingleOrder, db: Session = Depends(get_db)):
     order = pharmacy_purchase_order_service.submit(db=db, data_in=data_in)
     return handle_result (order)
