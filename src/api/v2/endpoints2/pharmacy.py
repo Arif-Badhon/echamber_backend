cc1271407c1e86d14737d7245bb3c254eaa9cdb2
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-20 18:02:52 +0600

    added: search by trade license

--- src/api/v2/endpoints2/pharmacy.py
+++ src/api/v2/endpoints2/pharmacy.py
@@ -14,3 +14,8 @@ router = APIRouter()
 def pharmacy_register_by_admin(data_in: PharmacyUserWithPharmacy, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin)):
     admin = pharmacy_service.register_pharmacy(db=db, data_in=data_in)
     return handle_result(admin)
+
+@router.get("/", response_model=PharmacyOut)
+def search_with_trade_license(trade_license: str, db: Session = Depends(get_db)):
+    trade = pharmacy_service.search_by_trade_license(db=db, trade_license=trade_license)
+    return handle_result(trade)
