06d0bf3aa545ca857e7d748ffb3747e180b82a9c
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-20 14:25:46 +0600

    removed: response model

--- src/api/v2/endpoints2/pharmacy_grn.py
+++ src/api/v2/endpoints2/pharmacy_grn.py
@@ -2,13 +2,12 @@ from fastapi import APIRouter, Depends
 from schemas import PharmacyGrnWithSingleGrn
 from db import get_db
 from sqlalchemy.orm import Session
-from schemas.pharmacy_grn import PharmacyGrnOut
 from services import pharmacy_grn_service
 from exceptions.service_result import handle_result
 
 router = APIRouter()
 
-@router.post("/", response_model=PharmacyGrnOut)
+@router.post("/")
 def grn_with_single_grn(data_in: PharmacyGrnWithSingleGrn, db: Session = Depends(get_db)):
     grn = pharmacy_grn_service.submit(db=db, data_in=data_in)
     return handle_result (grn)
