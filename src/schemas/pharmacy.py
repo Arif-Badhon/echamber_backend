2717a5ec53a76f2f68062474b498ed3a7a72554b
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-20 14:27:32 +0600

    fixed: spelling mistake

--- src/schemas/pharmacy.py
+++ src/schemas/pharmacy.py
@@ -5,11 +5,11 @@ from .users import UserCreateWitoutRole
 
 class PharmacyBase(BaseModel):
     name: str
-    trade_lisence: str
+    trade_license: str
     detail_address: Optional[str] = None
     district: Optional[str] = None
     sub_district: Optional[str] = None
-    drug_lisence: Optional[str] = None
+    drug_license: Optional[str] = None
     pharmacy_is_active: bool = False
 
 class PharmacyIn(PharmacyBase):
@@ -17,11 +17,11 @@ class PharmacyIn(PharmacyBase):
 
 class PharmacyUpdate(BaseModel):
     name: Optional[str] = None
-    trade_lisence: Optional[str] = None
+    trade_license: Optional[str] = None
     detail_address: Optional[str] = None
     district: Optional[str] = None
     sub_district: Optional[str] = None
-    drug_lisence: Optional[str] = None
+    drug_license: Optional[str] = None
     pharmacy_is_active: Optional[bool] = None
 
 class PharmacyOut(PharmacyBase):
