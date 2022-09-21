6570eb75b8b0ed04b8d1a041cabb452ba509ad19
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-21 18:07:22 +0600

    added: check identifier

--- src/services/pharmacy.py
+++ src/services/pharmacy.py
@@ -42,10 +42,24 @@ class PharmacyService(BaseService[Pharmacy, PharmacyIn, PharmacyUpdate]):
     def pharmacy_user_login(self, db: Session, data_in: PharmacyLogin):
         pharmacy_by_trade_license = self.search_by_trade_license(db=db, trade_license=data_in.trade_license)
         pharmacy_id = handle_result(pharmacy_by_trade_license).id
-        print(pharmacy_id)
+    
 
-        # users_repo
-        # search user by email or phone
+        user_from_identifier = None
+        user_email = users_repo.search_by_email(db=db, email_in=data_in.identifier)
+        user_phone = users_repo.search_by_phone(db=db, phone_in=data_in.identifier)
+        if user_email != None:
+            user_from_identifier = user_email
+        if user_phone != None:
+            user_from_identifier = user_phone
+
+
+        ph_check = pharmacy_user_service.get_by_two_key(db=db, skip=0, limit=100, descending=False, count_results=False, user_id=user_from_identifier.id, pharmacy_id=pharmacy_id)
+        ph_with_user = handle_result(ph_check)
+
+        if len(ph_with_user) == 0:
+            return ServiceResult(AppException.ServerError("Invalid pharmacy trade license or user."))
+
+
 
         return users_service.login(db=db, identifier=data_in.identifier, password=data_in.password)
 
