8879a561da1a1a0edd14c6f9450d54c3f2f734a3
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-09-26 17:06:49 +0600

    added: pharmacy stock

--- src/api/v2/routes.py
+++ src/api/v2/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from schemas import notice
-from .endpoints2 import pharmaceuticals, pharmacy, pharmacy_purchase_order, pharmacy_grn
+from .endpoints2 import pharmaceuticals, pharmacy, pharmacy_purchase_order, pharmacy_grn, pharmacy_stock
 
 api_router = APIRouter()
 
@@ -8,3 +8,4 @@ api_router.include_router(pharmaceuticals.router, prefix='/pharmaceuticals', tag
 api_router.include_router(pharmacy.router, prefix='/pharmacy', tags=['Pharmacy'])
 api_router.include_router(pharmacy_purchase_order.router, prefix='/pharmacy/purchase-order', tags=['Pharmacy Purchase Order'])
 api_router.include_router(pharmacy_grn.router, prefix='/pharmacy/grn', tags=['Pharmacy GRN'])
+api_router.include_router(pharmacy_stock.router, prefix='/pharmacy/stock', tags = ['PharmacyStock'])
