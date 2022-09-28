1e659e2486dac2f69c197476f2898d0d33777872
sajidurshajib <sajidur.inbox@gmail.com>
2022-09-28 18:07:52 +0600

    temp: pharmacy grn

--- src/services/pharmacy_grn.py
+++ src/services/pharmacy_grn.py
@@ -1,5 +1,3 @@
-from turtle import update
-from venv import create
 from services import BaseService
 from models import PharmacyGrn
 from schemas import PharmacyGrnIn, PharmacyGrnUpdate, PharmacyGrnWithSingleGrn, PharmacySingleGrnWithGrn, PharmacySingleGrnForStock, PharmacyTotalCurrentStockIn, PharmacyTotalCurrentStockUpdate
@@ -7,10 +5,11 @@ from repositories import pharmacy_grn_repo, pharmacy_single_grn_repo, pharmacy_e
 from sqlalchemy.orm import Session
 from exceptions.service_result import ServiceResult, handle_result
 
+
 class PharmacyGrnService(BaseService[PharmacyGrn, PharmacyGrnIn, PharmacyGrnUpdate]):
-    
+
     def submit(self, data_in: PharmacyGrnWithSingleGrn, db: Session):
-        grn = pharmacy_grn_repo.create_with_flush(db = db, data_in=PharmacyGrnIn(
+        grn = pharmacy_grn_repo.create_with_flush(db=db, data_in=PharmacyGrnIn(
             total_amount_dp=data_in.grn.total_amount_dp,
             grn_number=data_in.grn.grn_number,
             total_amount_mrp=data_in.grn.total_amount_mrp,
@@ -37,7 +36,7 @@ class PharmacyGrnService(BaseService[PharmacyGrn, PharmacyGrnIn, PharmacyGrnUpda
                     grn_id=grn.id
                 ))
 
-                single_stock = pharmacy_every_single_stock_repo.create(db=db, data_in=PharmacySingleGrnForStock(
+                single_stock = pharmacy_every_single_stock_repo.create_with_flush(db=db, data_in=PharmacySingleGrnForStock(
                     quantity=i.quantity,
                     expiry_date=i.expiry_date,
                     batch_number=i.batch_number,
@@ -46,21 +45,25 @@ class PharmacyGrnService(BaseService[PharmacyGrn, PharmacyGrnIn, PharmacyGrnUpda
                     pharmacy_id=data_in.grn.pharmacy_id
                 ))
 
-                check = pharmacy_every_single_stock_repo.get_by_two_key(db=db, skip=0, limit=100, descending=False, count_results=False, medicine_id =single_grn.id, pharmacy_id=grn.id)
-                check_grn_pharmacy = handle_result(check)
-                if not check_grn_pharmacy:
-                    totatl_stock = self.repo.create(db= db, data_in=PharmacyTotalCurrentStockIn(
-                        quantity=single_stock.quantity,
-                        medicine_id=single_stock.medicine_id,
-                        pharmacy_id=single_stock.pharmacy_id
+                check_medicine_exist = pharmacy_total_current_stock_repo.get_by_two_key(
+                    db=db, skip=0, limit=100, descending=False, count_results=True, medicine_id=i.medicine_id, pharmacy_id=data_in.grn.pharmacy_id)
+
+                if check_medicine_exist[0].results != 0:
+                    update_total = pharmacy_total_current_stock_repo.update(db=db, id=check_medicine_exist[1][0].id, data_update=PharmacyTotalCurrentStockUpdate(
+                        quantity=check_medicine_exist[1][0].quantity + i.quantity
                     ))
-                else:
-                    update_total = self.repo.update(db=db, id=single_grn.medicine_id.id, data_update=PharmacyTotalCurrentStockUpdate(
-                        quantity=+ single_stock.quantity
-                    ))
+                # check_grn_pharmacy = handle_result(check)
+                # if not check_grn_pharmacy:
+                #     totatl_stock = self.repo.create(db= db, data_in=PharmacyTotalCurrentStockIn(
+                #         quantity=single_stock.quantity,
+                #         medicine_id=single_stock.medicine_id,
+                #         pharmacy_id=single_stock.pharmacy_id
+                #     ))
+                # else:
 
         db.commit()
 
         return ServiceResult({"msg": "Success"}, status_code=200)
+
 
 pharmacy_grn_service = PharmacyGrnService(PharmacyGrn, pharmacy_grn_repo)
