ed3f8ba0c5181a5271bd9a04bc6d55c159de58e1
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-02 17:21:26 +0600

    added: pharmacy invoice and single invoice schemas

---
+++ src/schemas/pharmacy_invoice.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional
+
+from sqlalchemy import Float
+
+class PharmacyInvoiceBase(BaseModel):
+    subtotal_amount: Optional[float] = None
+    total_amount_mrp: Optional[float] = None
+    total_amount: Optional[float] = None
+    paid_amount: Optional[float] = None
+    due_amount: Optional[Float] = None
+    remarks: Optional[str] = None
+    discount: Optional[float] = None
+    vat: Optional[float] = None
+    invoice_number: Optional[str] = None
+    customer_id: Optional[int] = None
+    pharmacy_id: Optional[int] = None
+
+
+class PharmacyInvoiceIn(PharmacyInvoiceBase):
+    pass
+
+
+class PharmacyInvoiceUpdate(BaseModel):
+    subtotal_amount: Optional[float] = None
+    total_amount_mrp: Optional[float] = None
+    total_amount: Optional[float] = None
+    paid_amount: Optional[float] = None
+    due_amount: Optional[Float] = None
+    remarks: Optional[str] = None
+    discount: Optional[float] = None
+    vat: Optional[float] = None
+    invoice_number: Optional[str] = None
+    customer_id: Optional[int] = None
+    pharmacy_id: Optional[int] = None
+    
+
+class PharmacyInvoiceOut(PharmacyInvoiceBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+# Pharmacy Single Invoice
+
+class PharmacySingleInvoiceBase(BaseModel):
+    mrp: Optional[float] = None
+    quantity: Optional[int] = None
+    unit_prize: Optional[float] = None
+    discount: Optional[float] = None
+    cost: Optional[float] = None
+    medicine_id: Optional[int] = None
+    invoice_id: Optional[int] = None
+
+
+class PharmacySingleInvoiceIn(PharmacyInvoiceBase):
+    pass 
+
+
+class PharmacySingleInvoiceUpdate(BaseModel):
+    mrp: Optional[float] = None
+    quantity: Optional[int] = None
+    unit_prize: Optional[float] = None
+    discount: Optional[float] = None
+    cost: Optional[float] = None
+    medicine_id: Optional[int] = None
+    invoice_id: Optional[int] = None
+
+
+class PharmacySingleInvoiceOut(PharmacyInvoiceBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
