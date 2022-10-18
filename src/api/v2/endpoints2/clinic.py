58557738aa8a0a8471b3f72bd6872526cdbac5e3
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-18 16:41:02 +0600

    added: added: get clinic and register pharmacy by admin api

---
+++ src/api/v2/endpoints2/clinic.py
@@ -0,0 +1,22 @@
+from typing import List
+from fastapi import APIRouter, Depends
+from api.v1.endpoints.doctor_chambers import get
+from exceptions.service_result import handle_result
+from schemas import ClinicOut, ClinicUserWithClinic
+from db import get_db
+from sqlalchemy.orm import Session
+from services import clinic_service
+from api.v2.auth_dependcies import logged_in_admin
+
+router = APIRouter()
+
+@router.get("/", response_model=List[ClinicOut])
+def get_clinic(db: Session=Depends(get_db)):
+    get_all_clinic = clinic_service.get(db=db)
+    return handle_result(get_all_clinic)
+
+@router.post("/signup")
+def clinic_register_by_admin(data_in: ClinicUserWithClinic, db: Session = Depends(get_db), current_user: Session = Depends(logged_in_admin)):
+    admin = clinic_service.register_clinic(db=db, data_in=data_in)
+    return handle_result(admin)
+
