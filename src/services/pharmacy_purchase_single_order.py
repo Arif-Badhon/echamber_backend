864233e696c5e87ff2ed9924945d800949c13c36
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-10-23 12:54:39 +0600

    using the existing function to reduce code

--- src/services/pharmacy_purchase_single_order.py
+++ src/services/pharmacy_purchase_single_order.py
@@ -21,14 +21,7 @@ class PharmacyPurchaseSingleOrderService(BaseService[PharmacyPurchaseSingleOrder
     def all_single_order_by_purchase_num(self, db: Session, skip: int, limit: int, purchase_number: str):
         purchase_order_by_purchase_num = pharmacy_purchase_order_service.get_by_key(db=db, skip=skip, limit=limit, descending=True, count_results=True, purchase_number=purchase_number)
         purchase_order_id = handle_result(purchase_order_by_purchase_num)[1][0].id
-        all_purchase_single_order = self.get_by_key(db=db, skip=skip, limit=limit, descending=True, count_results=True, purchase_order_id =purchase_order_id)
-        all_single_order_data = []
-        for i in handle_result(all_purchase_single_order)[1]:
-            med_id = i.medicine_id
-            medicines = ep_medicines_list_repo.get_one(db=db, id=med_id)
-            i.medicine_name = medicines.name
-            i.medicine_generic = medicines.generic
-            all_single_order_data.append(i)
+        all_purchase_single_order = self.all_single_order(db=db, skip=skip, limit=limit, purchase_order_id=purchase_order_id)
         return all_purchase_single_order
 
 pharmacy_purchase_single_order_service = PharmacyPurchaseSingleOrderService(PharmacyPurchaseSingleOrder, pharmacy_purchase_single_order_repo)
