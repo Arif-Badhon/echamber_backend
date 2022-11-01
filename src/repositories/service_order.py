15230a8076c0c8c35fec2d55ada1f813f014d3f6
sajidurshajib <sajidur.inbox@gmail.com>
2022-11-01 17:23:20 +0600

    add: service order repo for filter

--- src/repositories/service_order.py
+++ src/repositories/service_order.py
@@ -1,6 +1,12 @@
-from models import ServiceOrder
+from models import ServiceOrder, User
 from schemas import ServiceOrderIn, ServiceOrderUpdate
 from repositories import BaseRepo
 
 
-service_order_repo = BaseRepo[ServiceOrder, ServiceOrderIn, ServiceOrderUpdate](ServiceOrder)
+class ServiceOrderRepo(BaseRepo[ServiceOrder, ServiceOrderIn, ServiceOrderUpdate]):
+
+    def service_with_patient(self, service_id: int, customer_name: str, customer_phone: str, address: str, service_name: str, order_date: str, order_status: str, skip: int, limit: int):
+        return
+
+
+service_order_repo = ServiceOrderRepo(ServiceOrder)
