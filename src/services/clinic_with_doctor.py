f4071b3ff5a90d405e593df851a93416bf76ea53
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-05 16:16:24 +0600

    added: check doctor exist or not

--- src/services/clinic_with_doctor.py
+++ src/services/clinic_with_doctor.py
@@ -1,11 +1,12 @@
-from services import BaseService, clinic_user_service
+from services import BaseService, clinic_user_service, roles_service
 from models import ClinicWithDoctor
-from schemas import ClinicWithDoctorIn, ClinicWithDoctorUpdate
-from repositories import clinic_with_doctor_repo
+from schemas import ClinicWithDoctorIn, ClinicWithDoctorUpdate, ClinicActivityIn
+from repositories import clinic_with_doctor_repo, clinic_activity_repo, users_repo
 from sqlalchemy.orm import Session
 from exceptions.service_result import handle_result
 from exceptions.service_result import ServiceResult
 from exceptions.app_exceptions import AppException
+from fastapi import status
 
 
 
@@ -39,9 +40,34 @@ class ClinicWithDoctorService(BaseService[ClinicWithDoctor, ClinicWithDoctorIn, 
         if doctor_with_clinic == True:
             return ServiceResult(AppException.ServerError("Already registered in this clinic"))
 
+        get_doctor = users_repo.get_one(db=db, id=doctor_user_id)
+        if not get_doctor:
+            return ServiceResult(AppException.ServerError("Doctor not exist."))
+        else:
+            doc_role = roles_service.role_id_by_name(db=db, name='doctor')
+            print(get_doctor.role_id)
+            if get_doctor.role_id != doc_role:
+                return ServiceResult(AppException.ServerError("Not valid doctor"))
+
+
+
+
         doc_append = clinic_with_doctor_repo.create_with_flush(db=db, data_in=ClinicWithDoctorIn(clinic_id=  clinic_id,doctor_id= doctor_user_id))
         
-        db.commit()
-        return ServiceResult({"msg": "Success"}, status_code=200)
+        doctor_append_data = ClinicActivityIn(
+            clinic_id = clinic_id,
+            user_id=user_id,
+            service_name="doctor_append",
+            service_received_id=doctor_user_id,
+            remark=""
+        )
+
+        doctor_append = clinic_activity_repo.create(db=db, data_in=doctor_append_data)
+
+        if not doctor_append:
+            return ServiceResult(AppException.ServerError("Problem with doctor append"))
+        else:
+            return ServiceResult(doctor_append, status_code=status.HTTP_201_CREATED)
+
 
 clinic_with_doctor_service = ClinicWithDoctorService(ClinicWithDoctor, clinic_with_doctor_repo)
