5728ef47f7088a490dbb8046a52413eb6a200570
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-06 12:02:19 +0600

    added: pagination

--- src/repositories/clinic_with_doctor.py
+++ src/repositories/clinic_with_doctor.py
@@ -6,7 +6,9 @@ from sqlalchemy.orm import Session
 
 class ClinicWithDoctorRepo(BaseRepo[ClinicWithDoctor, ClinicWithDoctorIn, ClinicWithDoctorUpdate]):
     
-    def search_by_clinic_id(self, db: Session, clinic_id: int):
-        return db.query(self.model, User).join(self.model, self.model.doctor_id ==  User.id).filter(self.model.clinic_id == clinic_id).all()
+    def search_by_clinic_id(self, db: Session, clinic_id: int, skip: int, limit: int):
+        data_count =  db.query(self.model, User).join(self.model, self.model.doctor_id ==  User.id).filter(self.model.clinic_id == clinic_id).all()
+        data =  db.query(self.model, User).join(self.model, self.model.doctor_id ==  User.id).filter(self.model.clinic_id == clinic_id).offset(skip).limit(limit).all()
+        return [{"results": len(data_count)}, data]
 
 clinic_with_doctor_repo = ClinicWithDoctorRepo(ClinicWithDoctor)
