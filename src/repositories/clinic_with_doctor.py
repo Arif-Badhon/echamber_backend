43d3f83ee129da4e08aab3f8bd94fceaf38a5f9a
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-06 15:05:51 +0600

    added: doctor details in search doctor

--- src/repositories/clinic_with_doctor.py
+++ src/repositories/clinic_with_doctor.py
@@ -1,5 +1,5 @@
 from repositories import BaseRepo
-from models import ClinicWithDoctor, User
+from models import ClinicWithDoctor, User, Doctor, DoctorQualification, DoctorSpeciality
 from schemas import ClinicWithDoctorIn, ClinicWithDoctorUpdate
 from sqlalchemy.orm import Session
 
@@ -7,8 +7,8 @@ from sqlalchemy.orm import Session
 class ClinicWithDoctorRepo(BaseRepo[ClinicWithDoctor, ClinicWithDoctorIn, ClinicWithDoctorUpdate]):
     
     def search_by_clinic_id(self, db: Session, clinic_id: int, skip: int, limit: int):
-        data_count =  db.query(self.model, User).join(self.model, self.model.doctor_id ==  User.id).filter(self.model.clinic_id == clinic_id).all()
-        data =  db.query(self.model, User).join(self.model, self.model.doctor_id ==  User.id).filter(self.model.clinic_id == clinic_id).offset(skip).limit(limit).all()
+        data_count =  db.query(self.model).filter(self.model.clinic_id == clinic_id).all()
+        data =  db.query(self.model, User, Doctor, DoctorQualification, DoctorSpeciality).join(self.model, self.model.doctor_id ==  User.id).filter(self.model.doctor_id == Doctor.user_id).filter(self.model.doctor_id == DoctorQualification.user_id).filter(self.model.doctor_id == DoctorSpeciality.user_id).filter(self.model.clinic_id == clinic_id).offset(skip).limit(limit).all()
         return [{"results": len(data_count)}, data]
 
 clinic_with_doctor_repo = ClinicWithDoctorRepo(ClinicWithDoctor)
