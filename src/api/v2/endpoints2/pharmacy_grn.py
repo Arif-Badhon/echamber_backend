f9ab9239a61ea5ba7eaf2a142f34b3dd1fc24550
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-07 17:47:13 +0600

    added: grn update api

--- src/api/v2/endpoints2/pharmacy_grn.py
+++ src/api/v2/endpoints2/pharmacy_grn.py
@@ -1,6 +1,6 @@
 from typing import List, Union
 from fastapi import APIRouter, Depends
-from schemas import PharmacyGrnWithSingleGrn, ResultInt, PharmacyGrnOut, PharmacySingleGrnWithMedicine
+from schemas import PharmacyGrnWithSingleGrn, ResultInt, PharmacyGrnOut, PharmacySingleGrnWithMedicine, PharmacyGrnUpdate, PharmacySingleGrnOut, PharmacySingleGrnUpdate
 from db import get_db
 from sqlalchemy.orm import Session
 from services import pharmacy_grn_service, pharmacy_single_grn_service
@@ -24,3 +24,15 @@ def search_grn(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
 def search_single_grn(id: int, skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     search_single_grn = pharmacy_single_grn_service.all_single_grn(db=db, skip=skip, limit=limit, grn_id = id)
     return handle_result(search_single_grn)
+
+
+@router.patch('/update-grn{id}', response_model=PharmacyGrnOut)
+def update_grn(id: int, data_update: PharmacyGrnUpdate, db: Session = Depends(get_db)):
+    update_grn = pharmacy_grn_service.update(db=db, data_update=data_update, id=id)
+    return handle_result(update_grn)
+
+
+@router.patch('/update-single-grn/{id}', response_model=PharmacySingleGrnOut)
+def update_single_grn(id: int, data_update: PharmacySingleGrnUpdate, db: Session = Depends(get_db)):
+    update_single_grn = pharmacy_single_grn_service.update(db=db, data_update=data_update, id=id)
+    return handle_result(update_single_grn)
