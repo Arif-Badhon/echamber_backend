991b941277fd591c653e839f0a4276c9eb181cdc
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-07 17:49:14 +0600

    added: update total stock api

--- src/api/v2/endpoints2/pharmacy_stock.py
+++ src/api/v2/endpoints2/pharmacy_stock.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from db import get_db
 from sqlalchemy.orm import Session
 from exceptions.service_result import handle_result
-from schemas import PharmacyEverySingleStockOutWithMedicine, PharmacyTotalCurrentStockWithMedicine
+from schemas import PharmacyEverySingleStockOutWithMedicine, PharmacyTotalCurrentStockWithMedicine, PharmacyTotalCurrentStockOut, PharmacyTotalCurrentStockUpdate
 from services import pharmacy_every_single_stock_servive, pharmacy_total_current_stock_service
 from typing import List
 
@@ -19,3 +19,9 @@ def get_single_stock(db: Session = Depends(get_db)):
 def get_total_stock(db: Session = Depends(get_db)):
     search_total = pharmacy_total_current_stock_service.total_current_stock(db=db)
     return handle_result(search_total)
+
+
+@router.patch('/update-total-stock/{id}', response_model=PharmacyTotalCurrentStockOut)
+def update_total_stock(id: int, data_update: PharmacyTotalCurrentStockUpdate, db: Session = Depends(get_db)):
+    update_total_stock = pharmacy_total_current_stock_service.update(db=db, data_update=data_update, id=id)
+    return handle_result(update_total_stock)
