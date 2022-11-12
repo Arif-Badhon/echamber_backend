1ef8c0fd21d70b2a402be4e07fc6203e93d953e8
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-12 12:42:45 +0600

    added: \pharmacy id

--- src/services/pharmacy_total_current_stock.py
+++ src/services/pharmacy_total_current_stock.py
@@ -6,10 +6,10 @@ from exceptions.service_result import handle_result
 from sqlalchemy.orm import Session
 
 class PharmacyTotalCurrentStockService(BaseService[PharmacyTotalCurrentStock, PharmacyTotalCurrentStockIn, PharmacyTotalCurrentStockUpdate]):
-    def total_current_stock(self, db: Session, skip: int, limit: int):
-        total_stock = self.get(db=db)
+    def total_current_stock(self, db: Session, pharmacy_id: int, skip: int, limit: int):
+        total_stock = self.get_by_key(db=db, skip=skip, limit=limit, descending=True, count_results=True, pharmacy_id = pharmacy_id)
         total_stock_data = []
-        for i in handle_result(total_stock):
+        for i in handle_result(total_stock)[1]:
             med_id = i.medicine_id
             medicines = ep_medicines_list_repo.get_one(db=db, id=med_id)
             i.medicine_name = medicines.name
