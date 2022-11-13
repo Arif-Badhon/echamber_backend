16b3c7bd7c8d1f9387ee3d84fe8f4735b3fc3713
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-13 15:24:14 +0600

    added: search by clinic id

--- src/repositories/clinic_navbar.py
+++ src/repositories/clinic_navbar.py
@@ -1,5 +1,12 @@
 from repositories import BaseRepo
 from models import ClinicNavbar
 from schemas import ClinicNavbarIn, ClinicDetailsUpdate
+from sqlalchemy.orm import Session
 
-clinic_navbar_repo = BaseRepo[ClinicNavbar, ClinicNavbarIn, ClinicDetailsUpdate](ClinicNavbar)
+
+class ClinicNavbarRepo(BaseRepo[ClinicNavbar, ClinicNavbarIn, ClinicDetailsUpdate]):
+    def get_nav_by_clinic_id(self, db: Session, clinic_id: int):
+        data =  db.query(self.model).filter(self.model.clinic_id == clinic_id).all()
+        return  data
+
+clinic_navbar_repo = ClinicNavbarRepo(ClinicNavbar)
