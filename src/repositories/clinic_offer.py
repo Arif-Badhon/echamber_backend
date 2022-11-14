1ed49499b809744b2c2637248226c5ffd87aae55
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-14 11:58:30 +0600

    added: get by clinic id

--- src/repositories/clinic_offer.py
+++ src/repositories/clinic_offer.py
@@ -1,6 +1,13 @@
 from repositories import BaseRepo
 from models import ClinicOffer
 from schemas import ClinicOfferIn, ClinicOfferUpdate
+from sqlalchemy.orm import Session
+
+class ClinicOfferRepo(BaseRepo[ClinicOffer, ClinicOfferIn, ClinicOfferUpdate]):
+
+    def get_clinic_offer_by_clinic_id(self, db: Session, clinic_id: int):
+        data =  db.query(self.model).filter(self.model.clinic_id == clinic_id).all()
+        return  data
 
 
-clinic_offer_repo = BaseRepo[ClinicOffer, ClinicOfferIn, ClinicOfferUpdate](ClinicOffer)
+clinic_offer_repo = ClinicOfferRepo(ClinicOffer)
--- src/repositories/clinic_services.py
+++ src/repositories/clinic_services.py
@@ -1,6 +1,14 @@
 from repositories import BaseRepo
 from models import ClinicServices
 from schemas import ClinicServicesIn, ClinicServicesUpdate
+from sqlalchemy.orm import Session
 
 
-clinic_service_repo = BaseRepo[ClinicServices, ClinicServicesIn, ClinicServicesUpdate](ClinicServices)
+class ClinicServicesRepo(BaseRepo[ClinicServices, ClinicServicesIn, ClinicServicesUpdate]):
+
+    def get_clinic_services_by_clinic_id(self, db: Session, clinic_id: int):
+        data =  db.query(self.model).filter(self.model.clinic_id == clinic_id).all()
+        return  data
+
+
+clinic_services_repo = ClinicServicesRepo(ClinicServices)
