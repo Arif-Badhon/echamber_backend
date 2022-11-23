5cd394a269e61045f370a3c024a04ecf5f8f2b17
ShishirDasNiloy <shishirdasniloy@gmail.com>
2022-11-23 15:33:22 +0600

    fixed: get all invoice in filter

--- src/repositories/pharmacy_invoice.py
+++ src/repositories/pharmacy_invoice.py
@@ -13,18 +13,30 @@ class PhamracyInvoiceRepo(BaseRepo[PharmacyInvoice, PharmacyInvoiceIn, PharmacyI
 
     def invoice_filter(self, db: Session, pharmacy_id: int, customer_id: int, start_date: str, end_date: str, single_date: str, skip: int, limit: int):
         
+        if start_date is None:
+            start_date = ''
+        if single_date is None:
+            single_date = ''
+
+        print(customer_id)
+        
         if customer_id is not None:
             data_count = db.query(self.model).filter(self.model.customer_id == customer_id).filter(self.model.pharmacy_id == pharmacy_id).all()
             data = db.query(self.model).filter(self.model.customer_id == customer_id).filter(self.model.pharmacy_id == pharmacy_id).offset(skip).limit(limit).all()
             return [{"results": len(data_count)}, data]
-        elif start_date is not None:
+        elif len(start_date) !=0 :
             data_count = db.query(self.model).filter(self.model.created_at.between(start_date, end_date)).filter(self.model.pharmacy_id == pharmacy_id).all()
             data = db.query(self.model).filter(self.model.created_at.between(start_date, end_date)).filter(self.model.pharmacy_id == pharmacy_id).offset(skip).limit(limit).all()
             return [{"results": len(data_count)}, data]
-        else:
+        elif len(single_date) != 0:
             data_count = db.query(self.model).filter(self.model.created_at.like(f"%{single_date}%")).filter(self.model.pharmacy_id == pharmacy_id).all()
             data = db.query(self.model).filter(self.model.created_at.like(f"%{single_date}%")).filter(self.model.pharmacy_id == pharmacy_id).offset(skip).limit(limit).all()
             return [{"results": len(data_count)}, data]
+        else:
+            data_count =  db.query(self.model).filter(self.model.pharmacy_id == pharmacy_id).all()
+            data =  db.query(self.model).filter(self.model.pharmacy_id == pharmacy_id).offset(skip).limit(limit).all()
+            return [{"results": len(data_count)}, data]
+
 
 
     def get_total_sale(self, db: Session, pharmacy_id: int, customer_id: int, start_date: str, end_date: str):
