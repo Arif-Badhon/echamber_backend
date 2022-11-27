96461f484f8aa07d48d2e6d6647d8d98d4a3bc08
sajidurshajib <sajidur.inbox@gmail.com>
2022-11-27 12:49:20 +0600

    fix

--- src/repositories/patients.py
+++ src/repositories/patients.py
@@ -18,7 +18,7 @@ class PatientRepo(BaseRepo[Patient, PatientIn, PatientUpdate]):
         db.query(self.model).filter(self.model.user_id == user_id).update(
             data_update.dict(exclude_unset=True), synchronize_session=False)
         db.commit()
-        return self.get_by_user_id(db, user_id=user_id)
+        return self.get_by_key_first(db, user_id=user_id)
 
 
 patients_repo = PatientRepo(Patient)
