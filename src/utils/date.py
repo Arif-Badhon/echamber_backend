25dc0a5c469b782b244638a4a7754b6244fdc816
morshedul-antor <antor.morshedul@gmail.com>
2023-01-15 14:50:54 +0600

    change: date format

--- src/utils/date.py
+++ src/utils/date.py
@@ -1,5 +1,6 @@
 from datetime import datetime
+today= datetime.today().strftime('%Y-%m-%d')
 
-current_date = datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
+current_date = today + 'T23:59:59'
 beginning_date = '2021-01-01'
 
