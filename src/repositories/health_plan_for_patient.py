8c46c18542dcf89fc78be0b32cd9c447f4746e79
morshedul-antor <antor.morshedul@gmail.com>
2023-01-26 13:13:09 +0600

    feat: new repo for plan with service_order and plan_list

--- src/repositories/health_plan_for_patient.py
+++ src/repositories/health_plan_for_patient.py
@@ -1,5 +1,16 @@
+from models.models import HealthPlanList
 from repositories import BaseRepo
 from models import HealthPlanForPatient
 from schemas import HealthPlanForPatientIn, HealthPlanForPatientUpdate
+from sqlalchemy.orm import Session
 
-health_plan_for_patient_repo = BaseRepo[HealthPlanForPatient, HealthPlanForPatientIn, HealthPlanForPatientUpdate](HealthPlanForPatient)
+class HealthPlanForPatientRepo(BaseRepo[HealthPlanForPatient, HealthPlanForPatientIn, HealthPlanForPatientUpdate]):
+
+    def health_plan_patient(self, db: Session, service_id: int):
+        query = db.query(HealthPlanList).filter(
+        HealthPlanForPatient.health_plan_id == HealthPlanList.id).filter(
+        HealthPlanForPatient.service_order_id == service_id).all()
+        
+        return query
+
+health_plan_for_patient_repo = HealthPlanForPatientRepo(HealthPlanForPatient)
